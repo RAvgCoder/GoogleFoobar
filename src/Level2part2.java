dd70727bfbc327f1e5161e76b412101e07bc7f1e
egbor <egborosebhulimen8@gmail.com>
2023-11-11 00:09:11 -0400

    Level2 part2 is done some comments added

--- src/Level2part2.java
+++ src/Level2part2.java
@@ -5,10 +5,10 @@
  * The bunnies are given food on standard-issue plates that are stamped with the numbers 0-9 for easier sorting, and you
  * need to combine sets of plates to create the numbers in the code. The signal that a number is part of the code is that it is
  * divisible by 3. You can do smaller numbers like 15 and 45 easily, but bigger numbers like 144 and 414 are a little trickier.
- * Write a program to help yourself quickly create large numbers for use in the code, given a limited number of plates to work with.You have L,
- * a list containing some digits (0 to 9). Write a function solution(L) which finds the largest number that can be made from some
+ * Write a program to help yourself quickly create large numbers for use in the code, given a limited number of plates to work with.You have numList,
+ * a list containing some digits (0 to 9). Write a function solution(numList) which finds the largest number that can be made from some
  * or all of these digits and is divisible by 3. If it is not possible to make such a number, return 0 as the solution.
- * L will contain anywhere from 1 to 9 digits.  The same digit may appear multiple times in the list, but each element in
+ * numList will contain anywhere from 1 to 9 digits.  The same digit may appear multiple times in the list, but each element in
  * the list may only be used once.
  * </p>
  * <p>
@@ -30,14 +30,14 @@ public class Level2part2 {
         System.out.println(solution(new int[]{1, 1, 7, 1, 1, 2})); // 0
     }
 
-    public static int solution(int[] num) {
+    public static int solution(int[] numList) {
         // Map Keys the index {0-9}, Values the frequency of the digits in said index
         int[] hashMap = new int[10];
 
-
+        
         int sum = 0;
         // Fill the map with the number frequency
-        for (int n : num) {
+        for (int n : numList) {
             hashMap[n]++;
             sum += n;
         }
