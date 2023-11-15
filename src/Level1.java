6373037ce01aaa42288ed328aa94005140d80b53
egbor <egborosebhulimen8@gmail.com>
2023-11-15 00:42:04 -0400

    Optimised the creation of prime strings

--- src/Level1.java
+++ src/Level1.java
@@ -35,48 +35,50 @@ public class Level1 {
      * Keeps track of the generated prime numbers.
      */
     static LinkedHashSet<Integer> primes = new LinkedHashSet<>();
+    static StringBuilder generatedPrimeString = new StringBuilder();
 
     public static void main(String[] args) {
-        System.out.println(solution(3));
+        System.out.println(solution(999999));
     }
 
     /**
      * Generates a string of prime numbers up to the specified index plus 5.
      *
-     * @param i The index to generate prime numbers up to.
+     * @param primeIndex The index to generate prime numbers up to.
      * @return A string containing the specified number of prime numbers.
      */
-    public static String solution(int i) {
-        StringBuilder generatedPrimeString = new StringBuilder();
-
-        // Generates the prime string up to the index specified + 5
-        for (int j = 1; j <= i + 5; j++) {
-            if (generatedPrimeString.length() > i + 5)
-                break;
-            generatedPrimeString.append(nthPrime(j));
-        }
+    public static String solution(int primeIndex) {
+        // Generates the prime string starting at the index specified + 5
+        nthPrime(primeIndex+5,primeIndex);
 
         // Extract the required numbers
-        return generatedPrimeString.substring(i, i + 5);
+        return generatedPrimeString.substring(primeIndex, primeIndex + 5);
     }
 
 
     /**
      * Generates the nth prime number.
      *
-     * @param p The index of the prime number to generate.
-     * @return The nth prime number.
+     * @param nthPrime The index of the prime number to generate.
+     * @param primeIndex The index of the prime number you are looking for
+     * been reached or false if it hasn't
      */
-    public static int nthPrime(int p) {
+    public static void nthPrime(int nthPrime, int primeIndex) {
         int num = 2;
-        while (p != 0) {
+        while (nthPrime != 0) {
             if (isPrime(num)) {
+                if (!primes.contains(num)) {
+                    generatedPrimeString.append(num);
+                    // Checks if value searching for has been reached
+                    if (generatedPrimeString.length() > primeIndex + 5)
+                        return;
+                }
                 primes.add(num);
-                p--;
+                nthPrime--;
             }
             num++;
         }
-        return --num;
+        --num;
     }
 
     /**
