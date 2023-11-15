5db1776d1fb236efc63030d066dcb7b3207c0e2d
egbor <egborosebhulimen8@gmail.com>
2023-11-15 00:42:46 -0400

    Code cleanup.

--- src/Level3part1.java
+++ src/Level3part1.java
@@ -73,46 +73,61 @@ import java.util.HashMap;
 public class Level3part1 {
     // HashMap to store already calculated results for specific subproblems
     private static final HashMap<String, Integer> memoizationMap = new HashMap<>();
-    // The total number of different staircases that can be built
-    private static int ans = -1;
+    /* The total number of different staircases that can be built: -1 as it adds [0,N] as a possibility,
+     * so we remove that
+     */
+    private static int totalNumberOfStaircases = 0;
 
     public static void main(String[] args) {
         // Test case: Calculate staircases for 200 bricks
         System.out.println("Number of staircases for 200 bricks: " + solution(200));
     }
 
-    // Function to calculate the number of different staircases
-    public static int solution(int n) {
-        generateDecisionTree(0, n);
-        return ans;
+    /**
+     * Function to calculate the number of different staircases
+     * @param numOfBlocksToUse Number of blocks you want to build your stairs with
+     * @return Number of ways to build those stairs
+     */
+    public static int solution(int numOfBlocksToUse) {
+        // Starts with zero height used and no blocks used
+        generateDecisionTree(0, numOfBlocksToUse);
+        return totalNumberOfStaircases-1;
     }
 
-    // Recursive function to generate the decision tree and calculate staircases
-    private static void generateDecisionTree(int lastStepNum, int remainingAfterPreviousStep) {
+    /**
+     * Recursive function to generate the decision tree and calculate staircases
+     * @param lastStairHeight The height of the last Stair that was built
+     * @param blocksRemaining The number of blocks left to build the stairs
+     */
+    private static void generateDecisionTree(final int lastStairHeight, final int blocksRemaining) {
         // Iterate through possible step heights
-        for (int currentStep = lastStepNum + 1; currentStep <= remainingAfterPreviousStep; currentStep++) {
-            int remainingAfterCurrentStep = (remainingAfterPreviousStep - currentStep);
+        for (int currentStairHeight = lastStairHeight + 1; currentStairHeight <= blocksRemaining; currentStairHeight++) {
+            final int remainingAfterCurrentStep = (blocksRemaining - currentStairHeight);
 
             // If no bricks remain, we have found a valid staircase
-            if (remainingAfterCurrentStep == 0) ans++;
-                // If the current step height is less than remaining bricks, continue generating the tree
-            else if (currentStep < remainingAfterCurrentStep) {
+            if (remainingAfterCurrentStep == 0) totalNumberOfStaircases++;
+
+            // If the current step height is less than remaining bricks, continue generating the tree
+            else if (currentStairHeight < remainingAfterCurrentStep) {
 
                 // Create a unique key to represent the subproblem
-                String subproblemKey = currentStep + "," + remainingAfterCurrentStep;
+                final String subProblemKey = currentStairHeight + "," + remainingAfterCurrentStep;
 
                 // Check if the result for this subproblem is already calculated
-                if (memoizationMap.containsKey(subproblemKey)) {
-                    ans += memoizationMap.get(subproblemKey);
+                if (memoizationMap.containsKey(subProblemKey)) {
+                    totalNumberOfStaircases += memoizationMap.get(subProblemKey);
                 } else {
-                    // Takes a snapshot of the current answer at current level before entering a new one
-                    int nodeAnswerSnapShot = ans;
+                    /*
+                     Takes a snapshot of the current numberOfStaircases
+                     computed at current level before entering a new one
+                     */
+                    final int nodeAnswerSnapShot = totalNumberOfStaircases;
 
                     // Generate decision trees for the current step
-                    generateDecisionTree(currentStep, remainingAfterCurrentStep);
+                    generateDecisionTree(currentStairHeight, remainingAfterCurrentStep);
 
                     // Store the result for this subproblem in the memoization map
-                    memoizationMap.put(subproblemKey, ans - nodeAnswerSnapShot);
+                    memoizationMap.put(subProblemKey, totalNumberOfStaircases - nodeAnswerSnapShot);
                 }
             }
         }
