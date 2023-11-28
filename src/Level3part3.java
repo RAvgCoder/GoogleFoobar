1385844ebe6a57f5ae4a85436d157020dba681ed
egbor <egborosebhulimen8@gmail.com>
2023-11-27 23:56:23 -0400

    Half way there

--- src/Level3part3.java
+++ src/Level3part3.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Doomsday Fuel=============
@@ -36,36 +37,115 @@ public class Level3part3 {
         // Your code here
         Matrix orderMatrix = new Matrix(m);
         orderMatrix.absorb();
-        Matrix identiy = Matrix.subset(orderMatrix,0,0,orderMatrix.IDENTITY_SIZE,orderMatrix.IDENTITY_SIZE);
-        orderMatrix.print();
-        orderMatrix.printC();
+        orderMatrix.print("Absorb");
+
+        // now separate into R,Q
+
+        Matrix subMatrixR = Matrix.subMatrix(orderMatrix, 0, orderMatrix.IDENTITY_SIZE-1, orderMatrix.IDENTITY_SIZE, orderMatrix._int.length);
+        subMatrixR.print("Sub_R");
+
+        Matrix subMatrixQ = findQ(orderMatrix);
+        subMatrixQ.print("Sub_Q");
+
+        // Calculate F {(I-Q)^-1}
+        Matrix identity = Matrix.identity(subMatrixQ.lenY());
+        identity.print("Identity");
+
+        Matrix I_Q = identity.sub(subMatrixQ);
+        I_Q.print("I_Q");
+
          int[] a = new int[1];
         return a;
 
+    }
+
+    private static Matrix findQ(Matrix orderMatrix) {
+        Matrix Q = Matrix.Matrix_float(Matrix.subMatrix(orderMatrix,orderMatrix.IDENTITY_SIZE-1,orderMatrix.IDENTITY_SIZE-1,orderMatrix._int.length,orderMatrix._int.length)._int);
+        int divisor = Arrays.stream(orderMatrix._int[orderMatrix._int.length-1]).sum();
+        for(int i = 1; i < Q._float.length; i++){
+            for (int j = 1; j < Q._float.length; j++) {
+                Q._float[i][j] /= divisor;
+            }
+        }
+        return Q;
     }
 
 
 }
 
 class Matrix{
-    int[][] num;
+    int[][] _int;
+    float[][] _float;
     int IDENTITY_SIZE = 0;
+    public int lenX(){
+        return ((_int != null) ?_int[0].length : _float[0].length) - ((IDENTITY_SIZE == -1) ? 0: 1);
+    }
+    public int lenY(){
+        return ((_int != null) ?_int : _float).length - ((IDENTITY_SIZE == -1) ? 0: 1);
+    }
 
-    public Matrix(int[][] n){
-        num = new int[n.length+1][n.length+1];
-        for (int i = 0; i < n.length; i++, num[0][i] = ('A'-1)+i, num[i][0] = ('A'-1)+i);
+    public static Matrix identity(int size){
+        Matrix m = new Matrix();
+        m.IDENTITY_SIZE = -1;
+        m._int = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            m._int[i][i] = 1;
+        }
+        return m;
+    }
+
+    private Matrix(){}
+
+    public static Matrix Matrix_float(int[][] n){
+        Matrix m = new Matrix();
+        m._float = new float[n.length][n[0].length];
 
         for (int i = 0; i < n.length; i++) {
-            for (int j = 0; j < n.length; j++) {
-                num[i+1][j+1] = n[i][j];
+            for (int j = 0; j < n[0].length; j++) {
+                m._float[i][j] = n[i][j];
             }
+        }
+        return m;
+    }
+
+    public Matrix(int[][] n){
+        _int = new int[n.length+1][n[0].length+1];
+
+        for (int i = 0; i < _int.length; i++){
+            _int[i][0] = ('A'-1)+i;
+        }
+
+        for (int i = 0; i < _int[0].length; i++) {
+            _int[0][i] = ('A'-1)+i;
+        }
+        _int[0][0] = '$';
+
+        for (int i = 0; i < n.length; i++) {
+            System.arraycopy(n[i], 0, _int[i + 1], 1, n[0].length);
         }
     }
 
+    public static Matrix subMatrix(Matrix orderMatrix, int startX, int startY, int endX, int endY) {
+        int[][] sub = new int[(endY-1)-startY][(endX-1)-startX];
+        int y = startY;
+        for (int i = 0; i < sub.length; i++) {
+            int x = startX;
+            for (int j = 0; j < sub[0].length; j++) {
+                sub[i][j] = orderMatrix.getNum(y,x);
+                x++;
+            }
+            y++;
+        }
+        Matrix res = new Matrix(sub);
+        // Copy over the letters
+        // Row
+        for (int i = 1; i < res._int[0].length; i++) {
+            res.setNum(-1,-1+i, orderMatrix.getNum(-1,-1+i));
+        }
 
-    public static Matrix subset(Matrix orderMatrix, int startX, int startY, int endX, int endY) {
-        int[][] sub = new int[endX-startX][endY-startY];
-        return new Matrix(sub);
+        // Column
+        System.arraycopy(orderMatrix._int[0], 0, res._int[0], 0, res._int.length);
+        return res;
     }
 
 
@@ -74,12 +154,12 @@ class Matrix{
         ArrayList<int[]> non_terminal = new ArrayList<>();
 
         // Make terminal and non-terminal
-        terminal.add(num[0]);
-        for (int i = 1; i < num.length; i++) {
-            int[] ints = num[i];
+        terminal.add(_int[0]);
+        for (int i = 1; i < _int.length; i++) {
+            int[] ints = _int[i];
             int sum = 0;
-            for (int j =1; j < num.length; j++) {
-                sum += num[i][j];
+            for (int j = 1; j < _int.length; j++) {
+                sum += _int[i][j];
             }
             if (sum == 0 || sum == 1) terminal.add(ints);
             else non_terminal.add(ints);
@@ -87,81 +167,137 @@ class Matrix{
 
         IDENTITY_SIZE = terminal.size();
 
-        print();
-
         int i =0;
         for (int[] e : terminal){
-            num[i] = e;
+            _int[i] = e;
             i++;
         }
 
         int snap = i;
         for (int[] ints : non_terminal) {
-            num[i++] = ints;
+            _int[i++] = ints;
         }
 
         // Fix naming
-        for (i = 0; i < num.length; i++) {
-            num[0][i] = num[i][0];
+        for (i = 0; i < _int.length; i++) {
+            _int[0][i] = _int[i][0];
         }
 
-        for (i = snap; i < num.length; i++) {
-            int[] clone = num[i].clone();
-            for (int j = 1; j < num[0].length; j++, snap++) {
-                num[i][findIndex(j)] = clone[j];
+        for (i = snap; i < _int.length; i++) {
+            int[] clone = _int[i].clone();
+            for (int j = 1; j < _int[0].length; j++, snap++) {
+                _int[i][findIndex(j)] = clone[j];
             }
         }
 
-
-
-        // now separate into I,O,R,Q
-        // I & 0
+        // TODO Fix if already has identity and is not aligned
+        // Create identity for terminal states
         i = 0;
         for (int[] row : terminal) {
             row[i++] = 1;
         }
-        num[0][0] = '$';
+        _int[0][0] = '$';
     }
 
+    private int getNum(int i, int j) {
+        return _int[i+1][j+1];
+    }
+    private void setNum(int i, int j, int val) {
+        _int[i+1][j+1] = val;
+    }
+
+
     private int findIndex(int j) {
-        for (int i = num.length-1; i >= 0; i--) {
-            if (('A'-1)+j == num[0][i])
+        for (int i = _int.length-1; i >= 0; i--) {
+            if (('A'-1)+j == _int[0][i])
                 return i;
         }
         throw new IllegalStateException("Shouldn't reach this point, index should be found");
     }
 
-    public void print() {
+    public void print(String name) {
         int j = 1;
-        for (int[] n :num) {
-            int i = 1;
-            for (int e : n) {
-                System.out.print(e+"\t");
-                if (i++ == IDENTITY_SIZE) System.out.print("|\t");
-            }
-            if (j++ == IDENTITY_SIZE){
+        System.out.println(name);
+        if (_int != null) {
+            for (int[] n : _int) {
+                int i = 1;
+                for (int e : n) {
+                    System.out.print(e + "\t");
+                    if (i++ == IDENTITY_SIZE) System.out.print("|\t");
+                }
+                if (j++ == IDENTITY_SIZE) {
+                    System.out.println();
+                    System.out.print("-\t".repeat(n.length + 1));
+                }
                 System.out.println();
-                System.out.print("-\t".repeat(n.length+1));
             }
-            System.out.println();
+        }else {
+            for (float[] n : _float) {
+                int i = 1;
+                for (float e : n) {
+                    System.out.print(e + "\t");
+                    if (i++ == IDENTITY_SIZE) System.out.print("|\t");
+                }
+                if (j++ == IDENTITY_SIZE) {
+                    System.out.println();
+                    System.out.print("-\t".repeat(n.length + 1));
+                }
+                System.out.println();
+            }
         }
         System.out.println();
     }
-    public void printC() {
+    public void printC(String name) {
         int j =1;
-        for (int[] n :num) {
-            int i = 1;
-            for (int e : n) {
-                System.out.print((char) e+"\t");
-                if (i++ == IDENTITY_SIZE) System.out.print("|\t");
-            }
-            if (j++ == IDENTITY_SIZE){
+        System.out.println(name);
+       if (_int != null) {
+            for (int[] n : _int) {
+                int i = 1;
+                for (int e : n) {
+                    System.out.print((char) e + "\t");
+                    if (i++ == IDENTITY_SIZE) System.out.print("|\t");
+                }
+                if (j++ == IDENTITY_SIZE) {
+                    System.out.println();
+                    System.out.print("-\t".repeat(n.length + 1));
+                }
                 System.out.println();
-                System.out.print("-\t".repeat(n.length+1));
             }
-            System.out.println();
+        }else {
+            for (float[] n : _float) {
+                int i = 1;
+                for (float e : n) {
+                    System.out.print((char) e + "\t");
+                    if (i++ == IDENTITY_SIZE) System.out.print("|\t");
+                }
+                if (j++ == IDENTITY_SIZE) {
+                    System.out.println();
+                    System.out.print("-\t".repeat(n.length + 1));
+                }
+                System.out.println();
+            }
         }
         System.out.println();
+    }
+
+    public Matrix sub(Matrix other) {
+        int t_i = (this.IDENTITY_SIZE == -1) ? 0 : 1;
+        int t_j = (this.IDENTITY_SIZE == -1) ? 0 : 1;
+        int o_i = (other.IDENTITY_SIZE == -1) ? 0 : 1;
+        int o_j = (other.IDENTITY_SIZE == -1) ? 0 : 1;
+
+        Matrix m = new Matrix();
+        m._float = new float[this.lenY()][this.lenX()];
+        m.IDENTITY_SIZE = -1;
+
+        for (int i = 0; i < _int.length; o_i++, t_i++, i++) {
+            for (int j = 0; j < _int.length; t_j++, o_j++, j++) {
+                m._float[i][j] = _int[t_i][t_j] - other._float[o_i][o_j];
+            }
+            t_j = (this.IDENTITY_SIZE == -1) ? 0 : 1;
+            o_j = (other.IDENTITY_SIZE == -1) ? 0 : 1;;
+        }
+        return m;
     }
 }
 
